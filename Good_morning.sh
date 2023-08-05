8534c64c0e67a6bbfd752b16360ec707ec50db00
Rihan Afsar <77212532+Rihan-afsar@users.noreply.github.com>
2023-08-05 20:16:00 +0530

    Update and rename Good to Good_morning.sh

--- Good_morning.sh
+++ Good_morning.sh
@@ -3,11 +3,11 @@ b=`echo $a|cut -d":" -f1`
 
 if [ $b -ge 0 -a $b -lt 12 ]
 then
-echo  Good Morning
+echo "Good Morning"
 elif [ $b -ge 12 -a $b -lt 16 ]
 then
-echo Good Afternoon
+echo "Good Afternoon"
 elif [ $b -ge 16 -a $b -lt 23 ]
 then
-echo Good Night
+echo "Good Night"
 fi
