9425134b2429a12f886961b3bfe46f979280664e
Rihan Afsar <77212532+Rihan-afsar@users.noreply.github.com>
2023-09-09 23:15:07 +0530

    Create relation.sh

---
+++ relation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+a=20
+b=30
+
+#Shell Relational Oprater
+
+if [ $a -eq $b ]
+then
+echo "a is equlat to b"
+fi
+
+if [ $a -ne $b ]
+then
+echo "a is not equlat to b"
+fi
+
+if [ $a -gt $b ]
+then
+echo "a is greter than b"
+fi
+
+if [ $a -lt $b ]
+then
+echo "a less than b"
+fi
+
+
+if [ $a -ge $b ]
+then
+echo "a is grether then equlat to b"
+fi
+
+if [ $a -le $b ]
+then
+echo "a is less then equal b"
+fi
