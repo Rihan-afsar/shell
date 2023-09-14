d80b4f546407ef4d65a9ac25f4979eb792b2f7c4
Rihan Afsar <77212532+Rihan-afsar@users.noreply.github.com>
2023-09-14 23:23:31 +0530

    Update s2.sh

--- s2.sh
+++ s2.sh
@@ -1,8 +1,8 @@
 echo "enter a num "
 read num
-if [ $num -le 6 ]
+if [ $num -le 10 ]
 then
-echo " less than or equal to 6"
+echo " less than or equal to 10"
 else
-echo "greater than 6"
+echo "greater than 10"
 fi
